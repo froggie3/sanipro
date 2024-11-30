8c69a43b80c9c4ed7b78a7220b15c9a525200fc7
froggie3 <yokkin.h@gmail.com>
2024-11-30 11:49:39 +0900

    PromptDifferenceDetectorを追加

---
+++ src/sanipro/diff.py
@@ -0,0 +1,22 @@
+from sanipro.common import MutablePrompt
+from sanipro.filters.utils import collect_same_tokens
+from collections.abc import Set
+
+
+class PromptDifferenceDetector:
+    """Detects the difference between the two prompts."""
+    before_num: int
+    after_num: int
+    reduced_num: int
+    duplicated: Set[str]
+
+    def __init__(self, before_process: MutablePrompt, after_process: MutablePrompt):
+        self.before_num = len(before_process)
+        self.after_num = len(after_process)
+        self.reduced_num = self.before_num - self.after_num
+
+        THRESHOULD = 1
+        self.duplicated = set()
+        for key, tokens in collect_same_tokens(before_process).items():
+            if len(tokens) > THRESHOULD:
+                self.duplicated.add(key)
