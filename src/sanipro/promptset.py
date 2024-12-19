32ae280a9190ae0c1c22e3c8075ebfcf5d39517c
froggie3 <yokkin.h@gmail.com>
2024-12-19 12:08:00 +0900

    promptsetを追加

---
+++ src/sanipro/promptset.py
@@ -0,0 +1,74 @@
+from abc import ABC, abstractmethod
+from collections.abc import Set
+
+from sanipro.abc import MutablePrompt, TokenInterface
+from sanipro.compatible import Self
+
+PromptSet = Set[TokenInterface]
+
+
+class SetCalculator(ABC):
+    """The interface for a prompt as a set of tokens,
+    where it allows user to the set operation."""
+
+    @abstractmethod
+    def do_math(self, a: PromptSet, b: PromptSet) -> PromptSet: ...
+
+
+class SetCalculatorWrapper:
+    """The wrapper class for the specific set operation instances.
+    Executes a set operation to the two prompt,
+    having two prompt instances."""
+
+    def __init__(self, calcurator: SetCalculator) -> None:
+        self._calcurator = calcurator
+
+    def do_math(
+        self, a: PromptSet | MutablePrompt, b: PromptSet | MutablePrompt
+    ) -> PromptSet:
+        return self._calcurator.do_math(set(a), set(b))
+
+    @classmethod
+    def create_from(cls, key: str) -> Self:
+        """Creates the instance from the key"""
+
+        calculator_classes = {
+            "union": UnionCalculator,
+            "diff": DifferenceCalculator,
+            "intersection": IntersectionCalculator,
+            "xor": SymmetricDifferenceCalculator,
+        }
+        try:
+            calculator_cls = calculator_classes[key]
+            return cls(calculator_cls())
+        except KeyError:
+            # TODO
+            raise
+
+
+class UnionCalculator(SetCalculator):
+    """Calculate the union of two prompts."""
+
+    def do_math(self, a: PromptSet, b: PromptSet) -> PromptSet:
+        return a | b
+
+
+class IntersectionCalculator(SetCalculator):
+    """Calculate the intersection of to prompts."""
+
+    def do_math(self, a: PromptSet, b: PromptSet) -> PromptSet:
+        return a & b
+
+
+class SymmetricDifferenceCalculator(SetCalculator):
+    """Calculate the symmetric difference of two prompts."""
+
+    def do_math(self, a: PromptSet, b: PromptSet) -> PromptSet:
+        return a ^ b
+
+
+class DifferenceCalculator(SetCalculator):
+    """Calculate the difference of two prompts."""
+
+    def do_math(self, a: PromptSet, b: PromptSet) -> PromptSet:
+        return a - b
