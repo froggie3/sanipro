c91cfa569b7228c1f19096e3ff9e40bc401c4693
froggie3 <yokkin.h@gmail.com>
2025-01-13 23:10:43 +0900

    トークンのフォーマットに関するテストを修正

--- tests/test_token.py
+++ tests/test_token.py
@@ -1,17 +1,29 @@
 import unittest
 
+from sanipro.converter_context import format_a1111_token, format_csv_token
 from sanipro.token import A1111Token, CSVToken
 
 
 class TestTokenInteractive(unittest.TestCase):
     def test_token(self):
-        self.assertEqual(A1111Token("42", 1.0).__str__(), "42")
-        self.assertEqual(A1111Token("42", 1.1).__str__(), "(42:1.1)")
-        self.assertEqual(A1111Token("42", 1.04).__str__(), "(42:1.04)")
-        self.assertEqual(A1111Token("42", 1.05).__str__(), "(42:1.05)")
+        test_cases = [
+            (A1111Token("42", 1.0), "42"),
+            (A1111Token("42", 1.1), "(42:1.1)"),
+            (A1111Token("42", 1.04), "(42:1.04)"),
+            (A1111Token("42", 1.05), "(42:1.05)"),
+        ]
+
+        for token, expected in test_cases:
+            self.assertEqual(format_a1111_token(token), expected)
 
 
 class TestTokenNonInteractive(unittest.TestCase):
     def test_token(self):
-        self.assertEqual(CSVToken("42", 1.0).__str__(), "42\t1.0")
-        self.assertEqual(CSVToken("42", 1.1).__str__(), "42\t1.1")
+        delimiter = "\t"
+        test_cases = [
+            (CSVToken("42", 1.0), "42\t1.0"),
+            (CSVToken("42", 1.1), "42\t1.1"),
+        ]
+
+        for token, expected in test_cases:
+            self.assertEqual(format_csv_token(delimiter)(token), expected)
