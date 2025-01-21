d22fed0cecdd91b2eccce2bd1b4878c44a15e90e
froggie3 <yokkin.h@gmail.com>
2025-01-22 07:31:04 +0900

    SortAllのテストを追加

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -4,7 +4,14 @@ from sanipro.filters.exclude import ExcludeCommand
 from sanipro.filters.mask import MaskCommand
 from sanipro.filters.reset import ResetCommand
 from sanipro.filters.roundup import RoundUpCommand
+from sanipro.filters.sort_all import SortAllCommand
 from sanipro.filters.translate import TranslateTokenTypeCommand
+from sanipro.filters.utils import (
+    sort_by_length,
+    sort_by_ord_sum,
+    sort_by_weight,
+    sort_lexicographically,
+)
 from sanipro.token import A1111Token, CSVToken
 
 Token = A1111Token
@@ -13,10 +20,6 @@ PROMPT = (
     Token("white background", 1.0),
     Token("red hair", 1.0),
     Token("jacket", 1.0),
-    Token("one side up", 1.0),
-    Token("happy", 1.0),
-    Token("long hair", 1.0),
-    Token("shirt", 1.0),
     Token("1girl", 1.0),
     Token("simple background", 1.0),
     Token("looking at viewer", 1.0),
@@ -70,6 +73,59 @@ class TestRoundUpCommand(unittest.TestCase):
         f = RoundUpCommand(2).execute_prompt
 
         self.assertIn(Token(":d", 1.26), f(PROMPT))
+
+
+class TestSortAllCommand(unittest.TestCase):
+    def test_execute_prompt(self):
+        FIXED_PROMPT = [
+            Token("shirt", 1.3),
+            Token("one side up", 1.0),
+            Token("long hair", 1.2),
+            Token("happy", 1.1),
+        ]
+        tests = [
+            (
+                sort_lexicographically,
+                [
+                    Token("happy", 1.1),
+                    Token("long hair", 1.2),
+                    Token("one side up", 1.0),
+                    Token("shirt", 1.3),
+                ],
+            ),
+            (
+                sort_by_weight,
+                [
+                    Token("one side up", 1.0),
+                    Token("happy", 1.1),
+                    Token("long hair", 1.2),
+                    Token("shirt", 1.3),
+                ],
+            ),
+            (
+                sort_by_length,
+                [
+                    Token("shirt", 1.3),
+                    Token("happy", 1.1),
+                    Token("long hair", 1.2),
+                    Token("one side up", 1.0),
+                ],
+            ),
+            (
+                sort_by_ord_sum,
+                [
+                    Token("happy", 1.1),
+                    Token("shirt", 1.3),
+                    Token("long hair", 1.2),
+                    Token("one side up", 1.0),
+                ],
+            ),
+        ]
+
+        for func, expected in tests:
+            with self.subTest(funcname=func.__name__, reversed=False):
+                f = SortAllCommand(func).execute_prompt
+                self.assertEqual(expected, f(FIXED_PROMPT))
 
 
 class TestTranslateTokenCommand(unittest.TestCase):
