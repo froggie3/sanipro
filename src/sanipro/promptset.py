579bf47302fc9fc9b7237edf9aa4ff08ca4d115d
froggie3 <yokkin.h@gmail.com>
2025-01-31 18:20:47 +0900

    SetCalculatorWrapperを削除 (クライアント側でやる)

--- src/sanipro/promptset.py
+++ src/sanipro/promptset.py
@@ -15,33 +15,6 @@ class SetCalculator(ABC):
         """Do the set operation."""
 
 
-class SetCalculatorWrapper:
-    """The wrapper class for the specific set operation instances.
-    Executes a set operation to the two prompt,
-    having two prompt instances."""
-
-    def __init__(self, calcurator: SetCalculator) -> None:
-        self._calcurator = calcurator
-
-    def do_math(self, a: Prompt, b: Prompt) -> Prompt:
-        return self._calcurator.do_math(a, b)
-
-    @classmethod
-    def create_from(cls, key: str | None = "union") -> Self:
-        """Creates the instance from the key"""
-
-        if key == "union":
-            return cls(UnionCalculator())
-        elif key == "inter":
-            return cls(IntersectionCalculator())
-        elif key == "diff":
-            return cls(DifferenceCalculator())
-        elif key == "symdiff":
-            return cls(SymmetricDifferenceCalculator())
-
-        return cls(UnionCalculator())
-
-
 class UnionCalculator(SetCalculator):
     """Calculate the union of two prompts."""
 
@@ -66,7 +39,10 @@ class SymmetricDifferenceCalculator(SetCalculator):
 class DifferenceCalculator(SetCalculator):
     """Calculate the difference of two prompts."""
 
-    def do_math(self, a: Prompt, b: Prompt, reverse=False) -> Prompt:
-        if not reverse:
+    def __init__(self, reverse=False) -> None:
+        self.reverse = reverse
+
+    def do_math(self, a: Prompt, b: Prompt) -> Prompt:
+        if not self.reverse:
             return tuple(set(a) - set(b))
         return tuple(set(b) - set(a))
